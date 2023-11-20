4e56db6fdaff703ad4ebbf214f4232bb98f3ebc2
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2023-11-20 15:56:12 +0000

    converrsion done

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score ="22";
+ console.log(typeof score);
+
+ //here we convert string into number
+ let valueInNumber = Number(score);
+ console.log(typeof valueInNumber);
+
+ //"33" =>33
+ //"33abc" => NaN not a number
+ //true=>1 ; false=>0
+
+ let isLoggedIn =1;
+ let booleanisLoggedIn =Boolean(isLoggedIn);
+ console.log(typeof booleanisLoggedIn);
+
+ //1=>true; 0=>false
+ //""=>false
+ //"shubh"=>true
+
+ let someNumber =33;
+ let stringnum = String(someNumber);
+ console.log(typeof stringnum);
