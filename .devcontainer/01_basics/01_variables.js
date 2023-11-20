504726212854506d1109c909fc5f08967945daa5
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2023-11-20 06:22:52 +0000

    variables file done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=1234;
+let name = "shubham";
+var accountpassword = "1234";
+accountcity="jaipur";
+
+//const not changed
+
+
+accountpassword ="3445";
+console.log(accountpassword)
+console.table([ accountId, name,accountpassword , accountcity])
