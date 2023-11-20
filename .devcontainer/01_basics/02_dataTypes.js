d3e505ab5d1399c3acd256fff8c0f4b9bd98ec3d
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2023-11-20 15:37:52 +0000

    datatypes done

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";// treat all js code as newer version
+// alert(3 + 3)// we are using node.js not browser
+
+let name ="shubham";
+let age = 21;
+let isLoggedIn= false;
+ 
+//datatypes primitive
+//number =>2 to power 53
+//bigint
+//string=> ""
+//boolean => true/false
+//null => standalone  value
+//undefined => 
+//symbol => unique
+
+console.log(typeof age);
