b0df9d4cbfef56873afa7b36ab9026424356a018
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2023-11-21 13:24:39 +0000

    Done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,23 @@
+// const score =400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 123.876
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds =1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++Maths++++++++//
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(6.4));
+console.log(Math.min(3,45,67,));
+console.log(Math.max(3,45,67,));
