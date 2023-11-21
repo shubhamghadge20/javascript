897e830a44e2c1b4925c8553173c462d57b168c6
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2023-11-21 11:09:48 +0000

    strings done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log("2">1);
+console.log("02">1);
+
+console.log(null >0);
+console.log(null==0);
+console.log(null >= 0);
+
+// === is check condition as well as datatype also
+console.log("2" === 2);
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name ="shubham";
+const repoCount =50;
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+//****declaration of strings*****//
+
+const  gameName = new String('shubh');
+
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log((gameName.toUpperCase()));
+
+// console.log(gameName.charAt(1));
+// console.log(gameName.indexOf('b'));
+
+// const newString = gameName.substring(0,2);
+// console.log(newString);
+
+const newStringOne =" shubham "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url ="https://shubh.com/shubh%10ghadge"
+console.log(url.replace ('%10','-'));
+console.log(url.includes('satya'));
