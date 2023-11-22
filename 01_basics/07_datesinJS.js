6b48cb720ebae5b0b04bb314b5f532ac34018869
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2023-11-22 06:50:19 +0000

    done

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -1,5 +1,20 @@
-let myDate = new Date()
+// let myDate = new Date()
 
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedate = new Date(2023,0,23);
+// let myCreatedate = new Date(2023,0,23, 5, 3);
+// let myCreatedate = new Date("2023-01-14")
+let myCreatedate = new Date("01-14-2023")
+// console.log(myCreatedate.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate =new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDate());
