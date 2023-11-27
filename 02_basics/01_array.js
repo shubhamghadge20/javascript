8b89e6317dd6995c3c4ba43f535bc3edb4a9b610
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2023-11-27 16:42:17 +0000

    array part 1 finish

---
+++ 02_basics/01_array.js
@@ -0,0 +1,21 @@
+const myArray = [1,2,3,4,5]
+
+
+console.log(myArray[1]);
+
+//method
+
+// myArray.push(6);
+// myArray.push(7);
+// myArray.pop();
+// console.log(myArray);
+// myArray.unshift(9);
+// myArray.shift();
+console.log(myArray.includes(9));
+console.log(myArray.indexOf(9));
+console.log(typeof myArray);
+
+
+// console.log(myArray.slice(1,3));
+// console.log(myArray.splice(1,3));
+
