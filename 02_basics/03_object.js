a4054eebcfd84d914071398d8035c5771eb114a3
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2023-11-28 14:39:07 +0000

    basics object done

---
+++ 02_basics/03_object.js
@@ -0,0 +1,13 @@
+//singleton
+//object.create
+
+//object literals
+const JsUser = {
+    name : "shubham",
+    age : 21,
+    location:"Lhasurne",
+    email:"shubham@google.com",
+    isLoggedIn: false,
+    lastLoginDays : ["monday" , "saturday"]
+}
+console.log(JsUser.email)
