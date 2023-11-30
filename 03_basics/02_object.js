807ef8421c93fead498f508b73c71a54f2180be5
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2023-11-30 13:40:46 +0000

    done

--- 03_basics/02_object.js
+++ 03_basics/02_object.js
@@ -1,14 +1,14 @@
 // when multiple argument passes 
-// function calculateCartPrice(...num1){
-//     return num1
-// }
-// console.log(calculateCartPrice(200,400,500))
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,400,500))
 // above passes multiple argument  to combine it in single array we use (...num1)
 
-// function calculateCartPrice(val1,val2,...num1){
-//     return num1
-// }
-// console.log(calculateCartPrice(200,400,500,2000))
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,400,500,2000))
 //above val1 get 200 , val2 get 400 so only 500,2000 print
 
 // below we object passes in function
