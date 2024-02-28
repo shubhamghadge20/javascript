b0f148db53c04ea356bf68541506b7aac7aec307
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2024-02-28 16:27:06 +0000

    scope done

---
+++ 03_basics/03_Scope.js
@@ -0,0 +1,11 @@
+//var a is gloabal scope  global scope can access any where
+var a =20;
+
+{//b,c,d are block scope
+
+    //means we in block scope we can access b,c,d only in scope ({} -scope) 
+  let  b=12;
+  let   c =23;
+  let   d=11;
+}
+console.log(d);
