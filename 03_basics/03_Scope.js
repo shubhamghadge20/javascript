f7a69d223d64436a3294b523449a9ce9ea79fc45
Shubham Ghadge <127418236+shubhamghadge20@users.noreply.github.com>
2024-02-29 08:30:29 +0000

    scope

--- 03_basics/03_Scope.js
+++ 03_basics/03_Scope.js
@@ -3,7 +3,7 @@ var a =20;
 
 {//b,c,d are block scope
 
-    //means we in block scope we can access b,c,d only in scope ({} -scope) 
+    //means  in block scope we can access b,c,d only in scope ({} -scope) 
   let  b=12;
   let  c =23;
   let  d=11;
